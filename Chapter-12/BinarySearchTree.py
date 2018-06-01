874048fc8291df7d0201ef1cce982b333e70c314
Vu Thanh Cong <vuthanhcong.ict@gmail.com>
2018-06-01 15:52:38 +0700

    Add BST

---
+++ Chapter-12/BinarySearchTree.py
@@ -0,0 +1,77 @@
+class TreeNode(object):
+	def __init__(self, key, left=None, right=None, parent=None):
+		self.key = key
+		self.left = left
+		self.right = right
+		self.parent = parent
+
+	def __str__(self):
+		return "({})".format(self.key)
+
+def TreeSearch(root, key):
+	if root is None or root.key == key:
+		return root
+
+	return TreeSearch(root.left, key) if key < root.key else TreeSearch(root.right, key)
+
+def IterativeTreeSearch(root, key):
+	while root is not None and key != root.key:
+		root = root.left if key < root.key else root.right
+	return root
+
+def TreeMinimum(root):
+	while root is not None and root.left is not None:
+		root = root.left
+	return root
+
+def TreeMaximum(root):
+	while root is not None and root.right is not None:
+		root = root.right
+	return root
+
+def TreeSuccessor(node):
+	if node is None:
+		return None
+	if node.right is not None:
+		return TreeMinimum(node.right)
+	y = node.parent
+	while y is not None and y.right == node:
+		node = y
+		y = y.parent
+	return y
+
+def InorderTreeWalk(root):
+	if root is None:
+		return
+	InorderTreeWalk(root.left)
+	print(root.key)
+	InorderTreeWalk(root.right)
+
+
+if __name__ == '__main__':
+	n2 = TreeNode(2)
+	n4 = TreeNode(4)
+	n3 = TreeNode(3, left=n2, right=n4)
+	n2.parent = n3
+	n4.parent = n3
+	n9 = TreeNode(9)
+	n13 = TreeNode(13, left=n9)
+	n9.parent = n13
+	n7 = TreeNode(7, right=n13)
+	n13.parent = n7
+	n6 = TreeNode(6, left=n2, right=n7)
+	n2.parent = n6
+	n7.parent = n6
+	n17 = TreeNode(17)
+	n20 = TreeNode(20)
+	n18 = TreeNode(18, left=n17, right=n20)
+	n17.parent = n18
+	n20.parent = n18
+	root = TreeNode(15, left=n6, right=n18)
+	n6.parent = root
+	n18.parent = root
+	InorderTreeWalk(root)
+
+	print(TreeSuccessor(n6))
+	print(TreeSuccessor(n13))
+
